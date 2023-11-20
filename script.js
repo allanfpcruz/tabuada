0ef61b38844abf232f1ee33e9455c6599089077b
allanfpcruz <137228336+allanfpcruz@users.noreply.github.com>
2023-11-20 12:29:18 -0300

    término da estilização

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ const tabuadaContainer = document.querySelector('#tabuada-container')
 const inputNumber = document.querySelector('#input-num')
 const search = document.querySelector('#search')
 const resContent = document.querySelector('#result')
+const reset = document.querySelector('#reset')
 
 //funções
 function tabuada() {
     for(let i = 1; i <= 10; i++) {
+        const tabuadaList = document.createElement('ul')
+        tabuadaList.classList.add('tabuada-list')
+        tabuadaContainer.appendChild(tabuadaList)
         const tabuadaHeader = document.createElement('li')
         tabuadaHeader.setAttribute('class', 'tabuada-header')
-        tabuadaHeader.classList.add('tabuada-list')
-        tabuadaContainer.appendChild(tabuadaHeader)
+        tabuadaList.appendChild(tabuadaHeader)
         tabuadaHeader.innerHTML = `Tabuada do ${i}`
-        const tabuadaList = document.createElement('ul')
-        tabuadaHeader.appendChild(tabuadaList)
         for (let c = 1; c <= 10; c++) {
             const tabuadaItem = document.createElement('li')
             tabuadaItem.setAttribute('class', 'tabuada-item')
@@ -24,23 +25,37 @@ function tabuada() {
 }
 
 function gerarTabuada() {
-    const tabuadaTitle = document.createElement('ul')
+    reset.classList.remove('none')
+    const tabuadaContent = document.createElement('ul')
+    tabuadaContent.classList.add('tabuada-list')
+    tabuadaContent.classList.add('generated-tabuada')
+    resContent.appendChild(tabuadaContent)
+    const tabuadaTitle = document.createElement('li')
     tabuadaTitle.setAttribute('class', 'tabuada-header')
-    tabuadaTitle.classList.add('tabuada-list')
-    resContent.appendChild(tabuadaTitle)
     tabuadaTitle.innerHTML = `tabuada do ${inputNumber.value}`
+    tabuadaContent.appendChild(tabuadaTitle)
     for(let i = 1; i <= 10; i++) {
         const tabuadaItem = document.createElement('li')
         tabuadaItem.setAttribute('class', 'tabuada-item')
-        tabuadaTitle.appendChild(tabuadaItem)
+        tabuadaContent.appendChild(tabuadaItem)
         tabuadaItem.innerHTML = `${inputNumber.value} x ${i} = ${inputNumber.value * i}`
     }
+}
+
+function limparTabuada() {
+    const generatedTabuada = document.querySelectorAll('.generated-tabuada')
+    generatedTabuada.forEach(tabuada => {
+        tabuada.classList.add('none')
+    });
 }
 
 //eventos
 document.addEventListener('onload', tabuada())
 
 search.addEventListener('click', (e) => {
-    e.preventDefault()
     gerarTabuada()
+})
+
+reset.addEventListener('click', (e) => {
+    limparTabuada()
 })
